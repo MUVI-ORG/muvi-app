610e2bd66a77c1a47436e8d291f3617270aa9600
Ana Paredes <ana@LAPTOP-7KDFK4JQ.localdomain>
2021-10-01 23:44:04 -0400

    changes to comingSoon, MovieResponse and MovieInformation

--- MuviApp/MuviApp/Models/MoviesResponse.cs
+++ MuviApp/MuviApp/Models/MoviesResponse.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 namespace MuviApp.Models
 {
     public class MoviesResponse
     {
+        [JsonPropertyName("items")]
+        public List<Movie> Movies { get; set; }
+
+        [JsonPropertyName("errorMessage")]
+        public string ErrorMessage { get; set; }
+
     }
 }
