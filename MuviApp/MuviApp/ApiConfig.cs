e4909f74d7c2177fc00539cebab4d346fd835709
Alexis Novas <alexismoises@hotmail.com>
2021-10-13 23:05:40 -0400

    [FIX] api key placeholder

--- MuviApp/MuviApp/ApiConfig.cs
+++ MuviApp/MuviApp/ApiConfig.cs
@@ -8,15 +8,15 @@ namespace MuviApp
     {
         public const string BaseAdress = "https://imdb-api.com";
 
-        //public const string ApiKey = "k_lr7ijm2d";
+        //public const string ApiKey = "PASTE_YOUR_API_KEY_HERE";
 
-        public const string MostPopularMovies = "/en/API/MostPopularMovies/k_lr7ijm2d";
+        public const string MostPopularMovies = "/en/API/MostPopularMovies/PASTE_YOUR_API_KEY_HERE";
 
-        public const string ComingSoon = "/en/API/ComingSoon/k_lr7ijm2d";
+        public const string ComingSoon = "/en/API/ComingSoon/PASTE_YOUR_API_KEY_HERE";
 
-        public const string Title = "/en/API/Title/k_lr7ijm2d/{movieId}";
+        public const string Title = "/en/API/Title/PASTE_YOUR_API_KEY_HERE/{movieId}";
 
-        public const string Trailer = "/en/API/Trailer/k_lr7ijm2d/{movieId}";
+        public const string Trailer = "/en/API/Trailer/PASTE_YOUR_API_KEY_HERE/{movieId}";
 
         public const string Actor = "/en/API/Name/PASTE_YOUR_API_KEY_HERE/{actorId}";
 
