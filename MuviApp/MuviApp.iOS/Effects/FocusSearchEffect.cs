4244d6579d12abaa529521ed63df78a97d525694
Ana Paredes <ana@LAPTOP-7KDFK4JQ.localdomain>
2021-10-15 11:32:55 -0400

    Add Effect to SearchBar

---
+++ MuviApp/MuviApp.iOS/Effects/FocusSearchEffect.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace MuviApp.iOS.Effects
+{
+    public class FocusSearchEffect : PlatformEffect
+    {
+        UIColor backgroundColor;
+        protected override void OnAttached()
+        {
+            try
+            {
+                Control.BackgroundColor = backgroundColor = UIColor.FromRGB(204, 153, 255);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            
+        }
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (Control.BackgroundColor == backgroundColor)
+                    {
+                        Control.BackgroundColor = UIColor.White;
+                    }
+                    else
+                    {
+                        Control.BackgroundColor = backgroundColor;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+    }
+}
